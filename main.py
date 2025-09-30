bc2b5ce041cc26169b8963408e9a56377daab335
naiarariboldi <166125988+naiarariboldi@users.noreply.github.com>
2025-09-29 22:09:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
+import tkinter as tk
+from tkinter import messagebox
+from database import DatabaseManager
+from gui import LoginWindow, MainWindow
+from logger import setup_logger, log_error
 
-from gui import Application
+class Application(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.withdraw()
+        setup_logger()
+
+        self.db_manager = DatabaseManager("perucas_diferentonas.db")
+        if not self.db_manager.connect():
+            messagebox.showerror("Erro de Conexão", "Não foi possível conectar ao banco.")
+            log_error("Falha ao conectar ao banco.")
+            self.destroy()
+            return
+
+        self.db_manager.create_products_table()
+        self.db_manager.create_users_table()
+
+        self.login_window = LoginWindow(self, self.db_manager, self.show_main_window)
+
+    def show_main_window(self):
+        self.main_window = MainWindow(self, self.db_manager)
+        self.main_window.protocol("WM_DELETE_WINDOW", self.on_main_window_close)
+
+    def on_main_window_close(self):
+        if messagebox.askokcancel("Sair", "Deseja realmente sair?"):
+            self.db_manager.disconnect()
+            self.main_window.destroy()
+            self.destroy()
 
 if __name__ == "__main__":
     app = Application()
     app.mainloop()
-
